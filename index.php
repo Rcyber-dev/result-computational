5e4d4b17c3479a551adbe6cfab7b864b6c3d9a66
Rcyber-dev <musahussainidantani@gmail.com>
2024-10-11 09:47:36 -0700

    Update and rename index.html to index.php

--- index.php
+++ index.php
@@ -1,3 +1,32 @@
+<?php
+session_start();
+include('includes/config.php');
+// if($_SESSION['alogin']!=''){
+// $_SESSION['alogin']='';
+// }
+if(isset($_POST['login']))
+{
+$uname=$_POST['username'];
+$password=md5($_POST['password']);
+$sql ="SELECT UserName,Password FROM admin WHERE UserName=:uname and Password=:password";
+$query= $dbh -> prepare($sql);
+$query-> bindParam(':uname', $uname, PDO::PARAM_STR);
+$query-> bindParam(':password', $password, PDO::PARAM_STR);
+$query-> execute();
+$results=$query->fetchAll(PDO::FETCH_OBJ);
+if($query->rowCount() > 0)
+{
+$_SESSION['alogin']=$_POST['username'];
+echo "<script type='text/javascript'> document.location = 'dashboard.php'; </script>";
+} else{
+    
+    echo "<script>alert('Invalid Details');</script>";
+
+}
+
+}
+
+?>
 
 <!DOCTYPE html>
 <html lang="en">
